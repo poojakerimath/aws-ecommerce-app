df06fa08c4e3d69f02e4e69bbc2f0aec6bdf70e8
poojakerimath <136485023+poojakerimath@users.noreply.github.com>
2026-08-28 07:14:07 +0530

    adda few changes again and again

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "<h1>Welcome to Girija's Ecommerce App!</h1><p>Running on Docker + AWS</p>"
+    return "<h1>Welcome to Girija's Ecommerce App!to Big Sale 50% OFF</h1><p>Running on Docker + AWS</p>"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
